7c8cbdf74aa30f1edb669327852cc0e5bc28990a
Nikunj Mistry <nikunjmistry@gmail.com>
2014-09-06 19:14:13 -0700

    NikunjProject1-CarFactory

---
+++ NikunjProject1/CarFactory.swift
@@ -0,0 +1,16 @@
+//
+//  CarFactory.swift
+//  NikunjProject1
+//
+//  Created by Radha Mistry on 9/6/14.
+//  Copyright (c) 2014 Nikunj Mistry. All rights reserved.
+//
+
+import Foundation
+
+class CarFactory {
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
--- NikunjProject1/main.swift
+++ NikunjProject1/main.swift
@@ -2,7 +2,7 @@
 //  main.swift
 //  NikunjProject1
 //
-//  Created by Radha Mistry on 9/6/14.
+//  Created by Nikunj Mistry on 9/6/14.
 //  Copyright (c) 2014 Nikunj Mistry. All rights reserved.
 //
 
@@ -10,3 +10,20 @@ import Foundation
 
 println("Hello, World!")
 
+var myFirstCar = CarFactory()
+
+myFirstCar.name = "Mustang"
+myFirstCar.color = "Red"
+myFirstCar.horsepower = 200
+myFirstCar.automaticOption = true
+
+println("My \(myFirstCar.name) is \(myFirstCar.color) and has \(myFirstCar.horsepower) horsepower")
+
+var mySecondCar = CarFactory()
+
+mySecondCar.name = "BMW 3 Series"
+mySecondCar.color = "Black"
+mySecondCar.horsepower = 230
+mySecondCar.automaticOption = false
+
+println("My \(mySecondCar.name) is \(mySecondCar.color) and has \(mySecondCar.horsepower) horsepower")
