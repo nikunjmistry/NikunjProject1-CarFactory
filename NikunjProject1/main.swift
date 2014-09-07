db4c2f8e91d8c865f24f7a8434f96e555ff4779d
Nikunj Mistry <nikunjmistry@gmail.com>
2014-09-06 17:53:46 -0700

    Initial Commit

---
+++ NikunjProject1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NikunjProject1
+//
+//  Created by Radha Mistry on 9/6/14.
+//  Copyright (c) 2014 Nikunj Mistry. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
